57df683641df1bf758ac425fc7b0ba3acda41f64
violetapugliese <violeta.pugliese@gmail.com>
2021-12-23 08:25:08 -0300

    adding sass and react-icons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/scss/bootstrap.scss';
 import App from './App';
 
 
