5aa5950648e41757490364bbb9726d7eb8cb40ce
violetapugliese <violeta.pugliese@gmail.com>
2022-02-14 19:23:06 -0300

    update institutionsService

--- src/pages/Instituciones/pages/CentrosMedicos/CentrosMedicos.jsx
+++ src/pages/Instituciones/pages/CentrosMedicos/CentrosMedicos.jsx
@@ -1,24 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import institutionsServices from '../../../../services/institutionsServices'
 import useAuth from '../../../../hooks/useAuth.js'
 
 export default function CentrosMedicos() {
     const { tokenUser } = useAuth();
 
-    const [institutions, setInstitutions] = useState();
+    const [institutions, setInstitutions] = useState([]);
 
-    // useEffect(() => {
-    //     const getInstitutions =
-    //         () => {
-    //             institutionsServices(tokenUser)
-    //         };
-    //     getInstitutions()
-    // }, []);
+    const getInstitutions = useCallback(
+        () => {
+            institutionsServices(tokenUser)
+                .then((res) => {
+                    const allInstitutions = res
+                    return allInstitutions;
+                })
+                .then((res) => {
+                    setInstitutions(res);
+                    return institutions
+                })
+                .catch((err) => { console.log(err) })
+        },
+        [institutions, tokenUser],
+    )
+
+    useEffect(() => {
+        getInstitutions()
+    }, [])
 
     return (
         <div>
             <h5>Centros Médicos</h5>
-            {institutions ? institutions.map((ins) => {
+            {institutions?.length > 0 ? institutions.map((ins) => {
                 return (
                     <p key={ins.id}>{ins.name}</p>
                 )
--- src/services/institutionsServices.js
+++ src/services/institutionsServices.js
@@ -1,26 +1,15 @@
-const URL = "http://128.201.239.7/portalpaciente/api/v1/institutions/all";
+import axios from "axios";
 
-export default function institutionsServices(tokenUser) {
-  let dataPromise;
-  fetch(URL, {
-    method: "GET",
+export default async function institutionsServices(tokenUser) {
+  const authAxios = axios.create({
     headers: {
-      // "Access-Control-Allow-Origin": "*",
-      accept: "application/json",
       Authorization: `Bearer ${tokenUser}`,
-    },
-  })
-    .then(function (response) {
-      console.log("response =", response);
-      return response.json();
-    })
-    .then(function (data) {
-      dataPromise = data;
-      console.log("data = ", data);
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
-
-  return dataPromise;
+    }
+  });
+  try {
+    const result = await authAxios.get(`/institutions/all`);
+    return result.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
