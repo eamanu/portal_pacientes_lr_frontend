65430097c9e4a3e6ffcacd3f733c2e94a28fc276
violetapugliese <violeta.pugliese@gmail.com>
2022-02-14 19:21:55 -0300

    fix CORS error

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+// import { environment } from './environments/environments.demo'
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// module.exports = (app) => {
+//   app.use(
+//     createProxyMiddleware("/institutions/all", {
+//       target: environment.baseURL,
+//       changeOrigin: true,
+//     })
+//   );
+// };
