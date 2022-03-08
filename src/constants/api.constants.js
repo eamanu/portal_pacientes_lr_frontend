09e305310440f7c33026e0110518042a07eda1d4
violetapugliese <violeta.pugliese@gmail.com>
2022-03-08 12:17:55 -0300

    adding api.constants file

---
+++ src/constants/api.constants.js
@@ -0,0 +1,26 @@
+//INSTITUCIONES
+import { environment } from "../environments/environments.demo";
+
+const baseUrl = environment.baseURL;
+
+export let LOGIN_HEADER = () => {
+    let header = {
+        "accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      }
+      return header;
+}
+
+export let AUTH_HEADER = (tokenId) => {
+    let header = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': `Bearer ${tokenId}`
+    }
+    return header;
+  }
+
+export const API_ENDPOINTS_LOGIN = `${baseUrl}/login`;
+
+export const API_ENDPOINTS_INSTITUCIONES = `${baseUrl}/instituciones/all`;
+    
