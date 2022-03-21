62d41ec8a38f8846c09d90d777dbaaf3b6b46062
violetapugliese <violeta.pugliese@gmail.com>
2022-03-21 11:57:27 -0300

    new background login/register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -35,77 +35,87 @@ function Login() {
     //     loginServiceFetch()
     // }
 
+    // Background
+    const  [ number, setNumber ] = useState(2)
+
+    useEffect(() => { 
+        setNumber(Math.floor(Math.random() * (6 - 0)) + 1);
+    }, [])
+    
+
     return (
-        <Container className='h-100 d-flex align-items-center justify-content-center'>
-            {/* <h1>Iniciar Sesión</h1> */}
-            <Row className='w-100 h-75 d-flex align-items-center justify-content-center'>
-                <Col xs={12} sm={5} lg={4} className='h-100 d-none d-sm-flex'>
-                    <div className='w-100 h-100 pic'></div>
-                </Col>
-                <Col xs={12} sm={7} lg={5} className="d-flex flex-column justify-content-around h-100">
-                    <h2>Iniciar sesión</h2>
-                    <Form className="form-group in" onSubmit={handleSubmit(onSubmit)}>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control
-                                name="email"
-                                type="text"
-                                className="form-control"
-                                {...register('email', {
-                                    required: {
-                                        value: true,
-                                        message: "El campo es requerido."
-                                    },
-                                    pattern: {
-                                        value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
-                                        message: "El formato ingresado no es válido"
-                                    }
-                                })}
-                                onChange={(e) => { setEmail(e.target.value) }}
-                            />
-                            {errors.email && <ErrorMessage><p>{errors.email.message}</p></ErrorMessage>}
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                name="password"
-                                type="password"
-                                className="form-control"
-                                {...register('password', {
-                                    required: {
-                                        value: true,
-                                        message: "El campo es requerido."
-                                    },
-                                    minLength: {
-                                        value: 3,
-                                        message: "La contraseña debe tener al menos 3 caracteres",
-                                    }
-                                })}
-                                onChange={(e) => { setPassword(e.target.value) }}
-                            />
-                            {errors.password && <ErrorMessage><p>{errors.password.message}</p></ErrorMessage>}
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label><input type="radio" /> Recordar usuario y contraseña</Form.Label>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>
-                                <Link to="/recuperar-contraseña">¿Olvidaste tu contraseña?</Link>
-                            </Form.Label>
-                        </Form.Group>
-                        <div className="d-flex flex-column align-items-end mt-3">
-                            <Button variant="danger" type="submit">
-                                Iniciar Sesión
-                            </Button>
-                            <button type="button" className="btn btn-ligth mt-3" >
-                                <Link to="/register" className="text-danger">Crear cuenta</Link>
-                            </button>
-                        </div>
-                    </Form>
+        <div className={`bg-container bg${number} h-100 w-100 d-flex align-items-center justify-content-center position-relative`}>
+            <div className="circle"></div>
+            <Container className='h-100 d-flex align-items-center justify-content-center z-index-1'>
+                {/* <h1>Iniciar Sesión</h1> */}
+                <Row className='w-100 h-75 d-flex align-items-sm-center justify-content-center'>
+                    <Col xs={12} sm={5} lg={4} className='h-100 d-none d-sm-flex'>
+                        <div className='w-100 h-100 pic'></div>
+                    </Col>
+                    <Col xs={12} sm={7} lg={5} className="d-flex flex-column justify-content-around h-100">
+                        <h2>Iniciar sesión</h2>
+                        <Form className="form-group in" onSubmit={handleSubmit(onSubmit)}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                    name="email"
+                                    type="text"
+                                    className="form-control"
+                                    {...register('email', {
+                                        required: {
+                                            value: true,
+                                            message: "El campo es requerido."
+                                        },
+                                        pattern: {
+                                            value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+                                            message: "El formato ingresado no es válido"
+                                        }
+                                    })}
+                                    onChange={(e) => { setEmail(e.target.value) }}
+                                />
+                                {errors.email && <ErrorMessage><p>{errors.email.message}</p></ErrorMessage>}
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    name="password"
+                                    type="password"
+                                    className="form-control"
+                                    {...register('password', {
+                                        required: {
+                                            value: true,
+                                            message: "El campo es requerido."
+                                        },
+                                        minLength: {
+                                            value: 3,
+                                            message: "La contraseña debe tener al menos 3 caracteres",
+                                        }
+                                    })}
+                                    onChange={(e) => { setPassword(e.target.value) }}
+                                />
+                                {errors.password && <ErrorMessage><p>{errors.password.message}</p></ErrorMessage>}
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label><input type="radio" /> Recordar usuario y contraseña</Form.Label>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>
+                                    <Link to="/recuperar-contraseña">¿Olvidaste tu contraseña?</Link>
+                                </Form.Label>
+                            </Form.Group>
+                            <div className="d-flex flex-column align-items-end mt-3">
+                                <Button variant="danger" type="submit">
+                                    Iniciar Sesión
+                                </Button>
+                                <button type="button" className="btn btn-ligth mt-3" >
+                                    <Link to="/register" className="text-danger">Crear cuenta</Link>
+                                </button>
+                            </div>
+                        </Form>
 
-                    {/* Login with fetch */}
+                        {/* Login with fetch */}
 
-                    {/* <Form className="form-group in d-none" onSubmit={handleSubmit(onSubmitF)}>
+                        {/* <Form className="form-group in d-none" onSubmit={handleSubmit(onSubmitF)}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email</Form.Label>
                             <Form.Control
@@ -153,9 +163,10 @@ function Login() {
                         </div>
                     </Form> */}
 
-                </Col>
-            </Row>
-        </Container>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
 
     )
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,22 +2,29 @@ import { Container, Row, Col, } from 'react-bootstrap';
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 
 function Register() {
-    
-    return (
-        <Container className='h-100 d-flex align-items-center justify-content-center'>
-            <Row className='w-100 h-75  d-flex align-items-center justify-content-center mb-5 mb-sm-0'>
-                <Col xs={12} sm={5} lg={4} className='h-100 d-none d-sm-flex'>
-                    <div className='w-100 h-100 pic'></div>
-                </Col>
-                <Col xs={12} sm={7} lg={5} className="h-100 d-flex flex-column justify-content-between">
-                    <h2>Registrarse</h2>
-                    <div className="d-flex align-items-center"  style={{height: "auto", minHeight: "80%",}}>
-                        <RegisterForm formularioUsuario={true} ></RegisterForm>
-                    </div>
-                </Col>
 
-            </Row>
-        </Container>
+    // Background
+    const number = Math.floor(Math.random() * (6 - 0)) + 1;
+
+    return (
+        <div className={`bg-container bg${number} h-100 w-100 d-flex align-items-center justify-content-center position-relative`}>
+            <div className="circle"></div>
+            <Container className='h-100 d-flex align-items-center justify-content-center z-index-1 mb-5 mb-sm-0'>
+                <Row className='w-100 h-75  d-flex align-items-center justify-content-center mb-5 mb-sm-0'>
+                    <Col xs={12} sm={5} lg={4} className='h-100 d-none d-sm-flex'>
+                        <div className='w-100 h-100 pic'></div>
+                    </Col>
+                    <Col xs={12} sm={7} lg={5} className="d-flex flex-column justify-content-between bg-third pb-3 pb-sm-0">
+                        <h2>Registrarse</h2>
+                        <div className="d-flex align-items-center" style={{ height: "auto", minHeight: "80%", }}>
+                            <RegisterForm formularioUsuario={true} ></RegisterForm>
+                        </div>
+                    </Col>
+
+                </Row>
+            </Container>
+        </div>
+
     )
 }
 
