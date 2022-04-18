f5c5021a87de2530d1f605a72f109ac416df5a10
violetapugliese <violeta.pugliese@gmail.com>
2022-04-18 19:37:56 -0300

    adding constants

--- src/constants/api.constants.js
+++ src/constants/api.constants.js
@@ -49,4 +49,103 @@ export const API_ENDPOINTS_GETMESSAGES = `${baseUrl}/getmessages`;
 
 export const API_ENDPOINTS_SETMESSAGESREAD = `${baseUrl}/setmessagesread`;
 
-export const API_ENDPOINTS_PATIENT = `${baseUrl}/patient`;
+// hce
+
+export const API_ENDPOINTS_ALLERGIES = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/allergies/${patient_id}`;
+  return url;
+}
+
+export const API_ENDPOINTS_ANTHROPOMETRICDATA = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/anthropometricData/${patient_id}`;
+  return url;
+}
+
+export const API_ENDPOINTS_FAMILYHISTORIES = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/familyHistories/${patient_id}`;
+  return url;
+}
+
+export const API_ENDPOINTS_CHRONIC = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/chronic/${patient_id}`;
+  return url;
+  
+}
+
+export const API_ENDPOINTS_TOOTHRECORDS = (institution_id, patient_id, tooth_sct_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/toothRecords/${patient_id}/tooth/${tooth_sct_id}`;
+  return url;
+  
+}
+
+export const API_ENDPOINTS_ACTIVEPROBLEMS = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/activeProblems/${patient_id}`;
+  return url;
+  
+}
+
+export const API_ENDPOINTS_HOSPITALIZATION = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/hospitalization/${patient_id}`;
+  return url;
+  
+}
+
+export const API_ENDPOINTS_SOLVEDPROBLEMS = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/solvedProblems/${patient_id}`;
+  return url;
+  
+}
+
+export const API_ENDPOINTS_VITALSIGNS = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/vitalSigns/${patient_id}`;
+  return url;
+  
+}
+
+export const API_ENDPOINTS_INMUNIZATION = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/immunizations/${patient_id}`;
+  return url;
+  
+}
+
+export const API_ENDPOINTS_MEDICATIONS = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/medications/${patient_id}`;
+  return url;
+  
+}
+
+export const API_ENDPOINTS_PERSONALHYSTORIES = (institution_id, patient_id) =>{
+  let url = `${baseUrl}/hcegeneral/${institution_id}/personalHistories/${patient_id}`;
+  return url;
+  
+}
+
+
+// export const API_ENDPOINTS_BASICDATA = (gender_id, identification_number, type_id) =>{
+//   let url = `${baseUrl}/patient/basicData?gender_id=${gender_id}&identification_number=${identification_number}&type_id=${type_id}`;
+//   return url;
+// }
+
+// export const API_ENDPOINTS_COMPLETEDATA = (gender_id, identification_number, type_id) =>{
+//   let url = `${baseUrl}/patient/completeData?gender_id=${gender_id}&identification_number=${identification_number}&type_id=${type_id}`;
+//   return url;  
+// }
+
+// export const API_ENDPOINTS_PATIENTBASICDATA = (gender_id, identification_number, type_id) =>{
+//   let url = `${baseUrl}/patient/basicData?gender_id=${gender_id}&identification_number=${identification_number}&type_id=${type_id}`;
+//   return url;
+  
+// }
+
+// export const API_ENDPOINTS_PATIENTCOMPLETEDATA = (gender_id, identification_number, type_id) =>{
+//   let url = `${baseUrl}/patient/completeData?gender_id=${gender_id}&identification_number=${identification_number}&type_id=${type_id}`;
+//   return url;
+  
+// }
+
+
+
+export const API_INMUNIZATIONS = `${baseUrl}/hcegeneral/1/immunizations/1`;
+
+
+
