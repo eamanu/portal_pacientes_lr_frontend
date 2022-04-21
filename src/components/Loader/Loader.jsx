dc0532e5310ffc30de2c23c0c38d16cafbd45ee6
violetapugliese <violeta.pugliese@gmail.com>
2022-04-21 09:14:15 -0300

    update loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = (prop) => {
     let isActive = prop.isActive
 
     return (
-        <div className={`loader_container ${isActive ? 'd-flex' : 'd-none'}`}>
+        <div className={`loader_container ${isActive ? 'd-block' : 'd-none'}`}>
             <Spinner animation="border" variant="danger" />
         </div>
     )
