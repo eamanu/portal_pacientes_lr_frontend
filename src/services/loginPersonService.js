0fb6e1a1793cd84ff1792914bb2543adfe2309af
violetapugliese <violeta.pugliese@gmail.com>
2022-04-21 09:13:26 -0300

    remove set parameters

--- src/services/loginPersonService.js
+++ src/services/loginPersonService.js
@@ -1,18 +1,23 @@
 import axios from "axios";
-import { API_ENDPOINTS_LOGIN, API_ENDPOINTS_LOGINPERSON, LOGIN_HEADER } from "../constants/api.constants";
+import {
+  API_ENDPOINTS_LOGIN,
+  API_ENDPOINTS_LOGINPERSON,
+  LOGIN_HEADER,
+} from "../constants/api.constants";
 import { post } from "./httpServices";
 
 // aL INTENTAR HACER CON AXIOS, NO RECONOCE HEADER CONTENT-TYPE
-export async function loginPersonService(username, password) { //hardcode
+export async function loginPersonService(username, password) {
+  //hardcode
   try {
-    const searchParams = new URLSearchParams({
-      username: "personprueba3@mail.com",
-      password: "123",
-    });
-    //     const searchParams = new URLSearchParams({
-    //   username: username,
-    //   password: password,
+    // const searchParams = new URLSearchParams({
+    //   username: "personprueba3@mail.com",
+    //   password: "123",
     // });
+    const searchParams = new URLSearchParams({
+      username: username,
+      password: password,
+    });
     let data = searchParams.toString();
     const promise = await post(API_ENDPOINTS_LOGINPERSON, LOGIN_HEADER(), data);
     return promise;
