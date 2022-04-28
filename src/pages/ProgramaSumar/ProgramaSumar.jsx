0dfa2b1a160de368696caf8594f0f97c9bbac6ef
violetapugliese <violeta.pugliese@gmail.com>
2022-04-28 20:49:18 -0300

    adding InformationCard + update service Sumar

--- src/pages/ProgramaSumar/ProgramaSumar.jsx
+++ src/pages/ProgramaSumar/ProgramaSumar.jsx
@@ -1,34 +1,68 @@
 import { useCallback, useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import usePatient from '../../hooks/usePatient';
 import DataNotFound from "../../components/DataNotFound";
 import Loader from "../../components/Loader";
 import sumarServices from "../../services/sumarServices";
+import InformationCard from "./InformationCard";
+import Swal from "sweetalert2";
+import { error } from "../../components/SwalAlertData";
 
 export default function ProgramaSumar() {
 
     const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([])
+    const p = usePatient()
+    const idnPatient = 46410766 //hardcode - should be  p.patient.identification_number
 
     const getData = useCallback(
-      (idn) => {
-        sumarServices(idn)
-        .then((res) => {
-            console.log('res', res)
-        })
-      },
-      [],
+        (idn) => {
+            sumarServices(idn)
+                .then((res) => {
+                    if (res.sumar_rows) {
+                        let info = res.sumar_rows
+                        setData(info)
+                        setLoading(false)
+                    } else {
+                        setLoading(false)
+                        throw new Error('Error al solicitar datos de programa SUMAR')
+                    }
+                })
+                .catch((err) => {
+                    console.log('err', err)
+                    Swal.fire(error('Error al solicitar datos de programa SUMAR'))
+                })
+        },
+        [],
     )
 
     useEffect(() => {
-        getData(46410766)
+        getData(idnPatient) //hardoced
     }, [])
-    
+
     return (
         <Container className='programa-sumar p-3'>
             <h5 className='section-title'>Programa Sumar</h5>
-            {loading ? 
-            <Loader isActive={loading}/> 
-            : <DataNotFound text="programa sumar"></DataNotFound>
-        }
+            {loading
+                ? <Loader isActive={loading} />
+                : <div className="mt-5">
+                    {data.length > 0 ? data.map((d, i) => {
+                        return <InformationCard
+                            key={i}
+                            date={d.fecha_comprobante}
+                            group={d.grupo}
+                            subgroup={d.subgrupo}
+                            description={d.descripcion}
+                            diagnosis={d.diagnostico}
+                            code={d.codigo}
+                            weight={d.peso}
+                            blood_pressure={d.tension_arterial}
+                        />
+                    })
+                        : <DataNotFound text="información en Programa Sumar" />
+                    }
+                </div>
+            }
         </Container>
     )
 }
