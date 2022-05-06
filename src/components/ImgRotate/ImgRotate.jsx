22f60b46537d3d1d04e8e68585f495c8ae1ac5fe
violetapugliese <violeta.pugliese@gmail.com>
2022-05-06 13:01:50 -0300

    update ImgRotate

--- src/components/ImgRotate/ImgRotate.jsx
+++ src/components/ImgRotate/ImgRotate.jsx
@@ -1,30 +1,44 @@
 import { useState } from "react";
-import notImg from '../../assets/statics/img-idn-not-found.jpg';
+import notImg from '../../assets/statics/no-image-found.png';
 import * as MdIcon from 'react-icons/md';
+import { environment } from "../../environments/environments.demo";
 
 const ImgRotate = ({ img }) => {
 
-    const nImg = notImg //hardcode
+    const baseUrl = environment.baseURL
+    const nImg = notImg 
     //rotate img
     const [rotate, setRotate] = useState(false);
     const [deg, setDeg] = useState(0);
-    const rotateImg = () => {
+    const rotateRigth = () => {
         setRotate(true)
         setDeg(deg + 90)
     }
+    const rotateLeft = () => {
+        setRotate(true)
+        setDeg(deg - 90)
+    }
 
     return (
-        <>
-            <div className="d-flex justify-content-end my-1 my-tooltip">
-                <button className='btn text-secondary btn-icon' onClick={(e) => rotateImg()}><MdIcon.MdRotateRight /></button>
+        <div className="mb-3 d-flex flex-column align-items-center">
+        <div className="d-flex w-100 justify-content-end">
+            <div className="my-tooltip">
+                <a className='text-secondary btn-icon' onClick={(e) => rotateLeft()}><MdIcon.MdRotateLeft /></a>
                 <span className="tiptext">
-                   Rotar 90º
+                   Rotar a la izquierda
                 </span>
             </div>
-            <div className="admin-patient__img-container">
-                <img className={`admin-patient__img ${rotate ? 'rotate-img' : ''}`} style={{ transform: `rotate(${deg}deg)` }} src={nImg} alt={`document patient ${nImg}`} />
+            <div className="my-tooltip">
+                <a className='text-secondary btn-icon' onClick={(e) => rotateRigth()}><MdIcon.MdRotateRight /></a>
+                <span className="tiptext">
+                   Rotar a la derecha
+                </span>
             </div>
-        </>
+        </div>
+            <div className="admin-patient__img-container">
+                <img className={`admin-patient__img ${rotate ? 'rotate-img' : ''}`} style={{ transform: `rotate(${deg}deg)` }} src={img ? `${baseUrl+img}` : nImg } alt={`document patient`} />
+            </div>
+        </div>
     )
 }
 
