7e8d44fa66f2a3f2a721a764239b76b58277e33b
violetapugliese <violeta.pugliese@gmail.com>
2022-05-12 12:55:53 -0300

    change 'datos institucionales'

--- src/pages/Instituciones/Instituciones.jsx
+++ src/pages/Instituciones/Instituciones.jsx
@@ -6,17 +6,18 @@ import InstitucionesRouter from "./InstitucionesRouter";
 
 export default function Instituciones() {
     const routes = SidebarData.instituciones;
-    
+
 
     return (
         <Container className='p-3' >
+            <h5 className="section-title mb-3">Instituciones</h5>
             <Row>
                 <Col className='switch-container'>
-                {routes.map((route) => {
-                    return(
-                        <NavLink key={route.path} className='me-2' activeClassName='active-switch' to={route.path}>{route.title}</NavLink>
-                    )
-                })}
+                    {routes.map((route) => {
+                        return (
+                            <NavLink key={route.path} className='me-2' activeClassName='active-switch' to={route.path}>{route.title}</NavLink>
+                        )
+                    })}
                 </Col>
             </Row>
             <InstitucionesRouter></InstitucionesRouter>
