51b497d0077d8c67e264ea7985a0e0aab9f31938
violetapugliese <violeta.pugliese@gmail.com>
2022-06-03 17:53:42 -0300

    fix: tooltip

--- src/components/ImgRotate/ImgRotate.jsx
+++ src/components/ImgRotate/ImgRotate.jsx
@@ -22,18 +22,8 @@ const ImgRotate = ({ img }) => {
     return (
         <div className="mb-3 d-flex flex-column align-items-center">
         <div className="d-flex w-100 justify-content-end">
-            <div className="my-tooltip">
                 <a className='text-secondary btn-icon' onClick={(e) => rotateLeft()}><MdIcon.MdRotateLeft /></a>
-                <span className="tiptext">
-                   Rotar a la izquierda
-                </span>
-            </div>
-            <div className="my-tooltip">
                 <a className='text-secondary btn-icon' onClick={(e) => rotateRigth()}><MdIcon.MdRotateRight /></a>
-                <span className="tiptext">
-                   Rotar a la derecha
-                </span>
-            </div>
         </div>
             <div className="admin-patient__img-container">
                 <img className={`admin-patient__img ${rotate ? 'rotate-img' : ''}`} style={{ transform: `rotate(${deg}deg)` }} src={img ? `${baseUrl+img}` : nImg } alt={`document patient`} />
